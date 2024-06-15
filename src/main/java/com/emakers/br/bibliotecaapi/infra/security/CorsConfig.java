df4d08d0e5031d5fceef3c551f103ea27b3e9cd8
gabrafo <gafagundesmesquita@gmail.com>
2024-06-15 13:27:25 -0300

    added cors

---
+++ src/main/java/com/emakers/br/bibliotecaapi/infra/security/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.emakers.br.bibliotecaapi.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Endereço padrão das aplicações React caso a API receba um View
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
