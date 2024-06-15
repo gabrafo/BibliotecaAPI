a6d1e084ac345f188e5c1961cc022dbd465f2938
gabrafo <gafagundesmesquita@gmail.com>
2024-06-15 13:27:58 -0300

    quick refactor: cors config

--- src/main/java/com/emakers/br/bibliotecaapi/infra/config/CorsConfig.java
+++ src/main/java/com/emakers/br/bibliotecaapi/infra/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.emakers.br.bibliotecaapi.infra.security;
+package com.emakers.br.bibliotecaapi.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
